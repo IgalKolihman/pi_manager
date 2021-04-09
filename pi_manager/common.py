92882832d248902ed6a1a26e1ff488794e4cf785
Igal Kolihman <igal.kolihman@gmail.com>
2021-04-09 11:42:31 +0300

    possible fix for the wallpaper setter

--- pi_manager/common.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-
-def exec_command(args: list):
-    out = subprocess.run(args, stdout=subprocess.PIPE, shell=True)
-    return out.stdout.strip().decode("utf-8")
--- pi_manager/set_color.py
+++ pi_manager/set_color.py
@@ -1,13 +1,54 @@
 import os
+import subprocess
+from urllib import request
 
-from pi_manager.common import exec_command
+from pi_manager.consts import MANAGER_MEDIA_DIR
+
+ARG_MAP = {
+    'feh': ['feh', ['--bg-center'], '%s'],
+    'gnome': ['gsettings',
+              ['set', 'org.gnome.desktop.background', 'picture-uri'],
+              'file://%s']
+}
+
+WM_BKG_SETTERS = {
+    'spectrwm': ARG_MAP['feh'],
+    'scrotwm': ARG_MAP['feh'],
+    'wmii': ARG_MAP['feh'],
+    'i3': ARG_MAP['feh'],
+    'awesome': ARG_MAP['feh'],
+    'awesome-gnome': ARG_MAP['gnome'],
+    'gnome': ARG_MAP['gnome'],
+    'ubuntu': ARG_MAP['gnome']
+}
+
+
+# def set_wallpaper(color: str):
+#     if color not in ["yellow", "green", "blue", "red"]:
+#         print(f'Color type does not exist. Requires on of: "yellow", "green", "blue" or "red". Not "{color}"')
+#
+#     gsettings_path = exec_command(["which", "gsettings"])
+#     command = f"{gsettings_path} set org.gnome.desktop.background picture-uri /home/user/Pictures/wallpapers/X"
+
+def get_image(image_url: str):
+    request.urlretrieve(image_url, MANAGER_MEDIA_DIR)
 
 
 def set_wallpaper(color: str):
     if color not in ["yellow", "green", "blue", "red"]:
-        print(f'color type does not exist. needs "yellow", "green", "blue", "red" not "{color}"')
+        raise ValueError(f'Color "{color}" does not exist. Requires: "yellow", "green", "blue" or "red.')
 
-    # TODO: this command doesn't work, check why. maybe a new approach is needed
-    gsettings_path = exec_command(["which", "gsettings"])
-    command = f"{gsettings_path} set org.gnome.desktop.background picture-uri /home/user/Pictures/wallpapers/X"
+    get_image(f"https://raw.githubusercontent.com/IgalKolihman/pi_manager/main/wallpapers/{color}.jpg")
 
+    # Try to find background setter
+    desktop_environ = os.environ.get('DESKTOP_SESSION', '')
+
+    if desktop_environ and desktop_environ in WM_BKG_SETTERS:
+        bkg_setter, args, pic_arg = WM_BKG_SETTERS.get(
+            desktop_environ, [None, None])
+
+    else:
+        bkg_setter, args, pic_arg = WM_BKG_SETTERS['spectrwm']
+
+    pargs = [bkg_setter] + args + [pic_arg % f"{MANAGER_MEDIA_DIR}{color}.jpg"]
+    subprocess.call(pargs)
